5c8df46bb9b94b0f02704e816fc049b520fc1c8e
CristianManu <adorelis0704@gmail.com>
2021-08-31 19:06:44 -0300

    Definicion de las listas y comienzo de implementacion del metodo de carga. Agregados archivos de texto correspondientes a las listas.

--- src/clases/Sistema.java
+++ src/clases/Sistema.java
@@ -1,5 +1,16 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Sistema {
+	private ArrayList<Usuario> usuarios = new ArrayList<>();
+	private ArrayList<Atraccion> atracciones = new ArrayList<>();
+	private ArrayList<Promocion> promociones = new ArrayList<>();
+	private ArrayList<InformeCompra> informes = new ArrayList<>();
+	
+	
+	
+	
+	
 	
 }
