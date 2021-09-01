188f016a4284d580eec2ade4d88ac0528d0a1506
CristianManu <adorelis0704@gmail.com>
2021-08-31 21:50:46 -0300

    Implementacion parcial de la carga de lista de usuarios. Falta implementacion de Tipo_De_Atraccion para continuar

--- src/clases/Sistema.java
+++ src/clases/Sistema.java
@@ -4,48 +4,56 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class Sistema {
-	
-	//Atributos
+
+	// Atributos
 	private ArrayList<Usuario> usuarios = new ArrayList<>();
 	private ArrayList<Atraccion> atracciones = new ArrayList<>();
 	private ArrayList<Promocion> promociones = new ArrayList<>();
 	private ArrayList<InformeCompra> informes = new ArrayList<>();
-	
-	//Manejo de archivos
+
+	// Manejo de archivos
 	private File archivo;
 	private BufferedReader br;
 	private FileReader fr;
-	
+
 	public Sistema() {
 		this.usuarios = new ArrayList<Usuario>();
 		this.atracciones = new ArrayList<Atraccion>();
 		this.promociones = new ArrayList<Promocion>();
 		this.informes = new ArrayList<InformeCompra>();
 	}
-	
-	private void cargaUsuarios(String rutaUsuarios) {
+
+	public void cargaUsuarios(String rutaUsuarios) {
 		try {
 			archivo = new File(rutaUsuarios);
 			fr = new FileReader(archivo);
 			br = new BufferedReader(fr);
-			
+
 			String linea;
 			while ((linea = br.readLine()) != null) {
-				this.usuarios.add(new Usuario());
-				
+				String [] parametros = linea.split("-");
+				this.usuarios.add(new Usuario(parametros[0], Integer.parseInt(parametros[1]), Double.parseDouble(parametros[2]), null));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
 		}
 	}
-	
+
 	public void cargarListas(String rutaUsuarios, String rutaAtracciones, String rutaPromociones) {
 		cargaUsuarios(rutaUsuarios);
 	}
+
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
 	
-	
-	
-	
-	
-	
+
 }
