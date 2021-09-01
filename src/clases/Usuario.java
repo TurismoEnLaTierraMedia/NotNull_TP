5ed2e3cdc9d5d8a1c741d063c65b68b46d13d281
julianandresrebot <julianandresrebot@192.168.0.12>
2021-08-31 21:04:09 -0300

    Definición de clase usuario con su constructor

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -1,7 +1,33 @@
 package clases;
 
 public class Usuario {
-	
-	private String nombre;
 
+	private String nombre;
+	private double presupuesto;
+	private int tiempoDisponible;
+	private String tipoPreferido;
+
+	public Usuario(String nombre, double presupuesto, int tiempoDisponible, String tipoPreferido) {
+		super();
+		this.nombre = nombre;
+		this.presupuesto = presupuesto;
+		this.tiempoDisponible = tiempoDisponible;
+		this.tipoPreferido = tipoPreferido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getPresupuesto() {
+		return presupuesto;
+	}
+
+	public int getTiempoDisponible() {
+		return tiempoDisponible;
+	}
+
+	public String getTipoPreferido() {
+		return tipoPreferido;
+	}
 }
