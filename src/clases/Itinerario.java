3f2bb5c757ba5fd6fb7e715709cf7ccfbe96f0d1
CristianManu <adorelis0704@gmail.com>
2021-09-12 23:07:30 -0300

    Corregida la declaracion de tipo de totalPagar. double --> int. Tambien se implementaron sets en el constructor

--- src/clases/Itinerario.java
+++ src/clases/Itinerario.java
@@ -1,70 +1,71 @@
 package clases;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
 public class Itinerario {
-	private double totalPagar, tiempoNecesario;
+	private double tiempoNecesario;
+	private int totalPagar;
 	private Usuario usuario;
 	List<Promocion> promocionesCompradas;
 	List<Atraccion> atraccionesCompradas;
 
-	public Itinerario(double totalPagar, double tiempoNecesario, Usuario usuario, List<Promocion> promocionesCompradas,
+	public Itinerario(Usuario usuario, List<Promocion> promocionesCompradas,
 			List<Atraccion> atraccionesCompradas) {
-		this.totalPagar = totalPagar;
-		this.tiempoNecesario = tiempoNecesario;
 		this.usuario = usuario;
 		this.promocionesCompradas = promocionesCompradas;
 		this.atraccionesCompradas = atraccionesCompradas;
+		setTotalPagar();
+		setTiempoNecesario(tiempoNecesario);
 	}
 
-	public Itinerario() {
-		this.totalPagar = 0;
-		this.tiempoNecesario = 0;
-		this.usuario = null;
-		this.promocionesCompradas = null;
-		this.atraccionesCompradas = null;
-	}
-
-	protected double getTotalPagar() {
+	protected int getTotalPagar() {
 		return totalPagar;
 	}
 
-	protected void setTotalPagar(double totalPagar) {
-		this.totalPagar = totalPagar;
+	private void setTotalPagar() {
+		this.totalPagar = 0;
+		for (Iterator<Atraccion> iterator = atraccionesCompradas.iterator(); iterator.hasNext();) {
+			Atraccion atraccion = (Atraccion) iterator.next();
+			this.totalPagar += atraccion.getCostoDeVisita();
+		}
+		for (Iterator<Promocion> iterator = promocionesCompradas.iterator(); iterator.hasNext();) {
+			Promocion promocion = (Promocion) iterator.next();
+			this.totalPagar += promocion.obtenerPrecioFinal();
+		}
+		
 	}
 
 	protected double getTiempoNecesario() {
 		return tiempoNecesario;
 	}
 
-	protected void setTiempoNecesario(double tiempoNecesario) {
-		this.tiempoNecesario = tiempoNecesario;
+	private void setTiempoNecesario(double tiempoNecesario) {
+		this.tiempoNecesario = 0;
+		for (Iterator<Atraccion> iterator = atraccionesCompradas.iterator(); iterator.hasNext();) {
+			Atraccion atraccion = (Atraccion) iterator.next();
+			this.tiempoNecesario += atraccion.getDuracion();
+		}
+		for (Iterator<Promocion> iterator = promocionesCompradas.iterator(); iterator.hasNext();) {
+			Promocion promocion = (Promocion) iterator.next();
+			this.tiempoNecesario += promocion.getTiempoTotal();
+		}
 	}
 
 	protected Usuario getUsuario() {
 		return usuario;
 	}
 
-	protected void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
 	protected List<Promocion> getPromocionesCompradas() {
 		return promocionesCompradas;
 	}
 
-	protected void setPromocionesCompradas(List<Promocion> promocionesCompradas) {
-		this.promocionesCompradas = promocionesCompradas;
-	}
 
 	protected List<Atraccion> getAtraccionesCompradas() {
 		return atraccionesCompradas;
 	}
 
-	protected void setAtraccionesCompradas(List<Atraccion> atraccionesCompradas) {
-		this.atraccionesCompradas = atraccionesCompradas;
-	}
 
 	@Override
 	public int hashCode() {
