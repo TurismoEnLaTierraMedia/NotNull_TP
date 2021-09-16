256b02ac03d4b98f0cae075ff940b04ac97180c0
julianandresrebot <julianandresrebot@192.168.0.12>
2021-09-15 22:40:09 -0300

    Se agregó metodo para controlar preferencia de usuario. Se agregaron algunos test.

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.Objects;
 
 public class Usuario {
-	
+
 	private ArrayList<Atraccion> comprasAtracciones = new ArrayList<>();
 	private ArrayList<Promocion> comprasPromociones = new ArrayList<>();
 
@@ -14,13 +14,11 @@ public class Usuario {
 	private double tiempoDisponible;
 	private Tipo_De_Atraccion preferencia;
 
-	
-
 	public Usuario(String nombre, int presupuesto, double tiempoDisponible, String preferencia) {
 		this.nombre = nombre;
-		this.presupuesto = presupuesto;
-		this.tiempoDisponible = tiempoDisponible;
-		this.preferencia = Tipo_De_Atraccion.valueOf(preferencia);
+		this.setPresupuesto(presupuesto);
+		this.setTiempoDisponible(tiempoDisponible);
+		this.setPreferencia(preferencia);
 	}
 
 	public String getNombre() {
@@ -31,23 +29,40 @@ public class Usuario {
 		return presupuesto;
 	}
 
-	
-	public Tipo_De_Atraccion getPreferencia() {
-		return preferencia;
+	private void setPresupuesto(int presupuesto) {
+		if (presupuesto <= 0) {
+			throw new IllegalArgumentException("No se permiten números negativos");
+		} else {
+			this.presupuesto = presupuesto;
+		}
 	}
-	
+
+	public Tipo_De_Atraccion getPreferencia() {
+		return this.preferencia;
+	}
+
+	private void setPreferencia(String preferencia) {
+		try {
+			this.preferencia = Tipo_De_Atraccion.valueOf(preferencia.toUpperCase().replace("Ó", "O"));
+
+		} catch (IllegalArgumentException ex) {
+			System.out.println(preferencia + " no corresponde con un tipo de atracción");
+		}
+	}
+
 	/**
-	 * Método que revisa las promociones adquiridas por el usuario,
-	 * para no ofrecer atracciones que ya se encuentren en las mismas. 
+	 * Método que revisa las promociones adquiridas por el usuario, para no ofrecer
+	 * atracciones que ya se encuentren en las mismas.
+	 * 
 	 * @param atraccion
 	 * @return boolean
 	 */
-	
+
 	public boolean chequearAtraccionEnPromociones(Atraccion atraccion) {
 		Iterator<Promocion> promociones = this.getComprasPromociones().iterator();
 		while (promociones.hasNext()) {
 			Promocion promocion = (Promocion) promociones.next();
-			
+
 			ArrayList<Atraccion> atraccionesDePromociones = promocion.getAtracciones();
 //			for (int i = 0; i < atraccionesDePromociones.length; i++) {
 //				if (atraccion.equals(atraccionesDePromociones[i])) {
@@ -59,37 +74,37 @@ public class Usuario {
 					return true;
 				}
 			}
-			
+
 		}
 		return false;
 	}
-	
+
 	/**
-	 * Añade una Atracción a la lista de atraciones compradas
-	 * por el usuario, y modifica los atributos correspondientes
-	 * (presupuesto y tiempo disponible)
+	 * Añade una Atracción a la lista de atraciones compradas por el usuario, y
+	 * modifica los atributos correspondientes (presupuesto y tiempo disponible)
+	 * 
 	 * @param atraccion
 	 */
-	
+
 	public void comprarAtraccion(Atraccion atraccion) {
 		comprasAtracciones.add(atraccion);
 		this.presupuesto -= atraccion.getCostoDeVisita();
-		this.tiempoDisponible -= atraccion.getDuracion();	
+		this.tiempoDisponible -= atraccion.getDuracion();
 	}
-	
+
 	/**
-	 * Añade una Promoción a la lista de promociones compradas
-	 * por el usuario y modifica los atributos correspondientes
-	 * (presupuesto y tiempo disponible)
+	 * Añade una Promoción a la lista de promociones compradas por el usuario y
+	 * modifica los atributos correspondientes (presupuesto y tiempo disponible)
+	 * 
 	 * @param promocion
 	 */
-	
+
 	public void comprarPomocion(Promocion promocion) {
 		comprasPromociones.add(promocion);
 		this.presupuesto -= promocion.obtenerPrecioFinal();
 		this.tiempoDisponible -= promocion.getTiempoTotal();
 	}
-	
+
 	public ArrayList<Atraccion> getComprasAtracciones() {
 		return comprasAtracciones;
 	}
@@ -101,18 +116,28 @@ public class Usuario {
 	public double getTiempoDisponible() {
 		return tiempoDisponible;
 	}
-	
+
+	private void setTiempoDisponible(double tiempoDisponible) {
+		if (tiempoDisponible <= 0) {
+			throw new IllegalArgumentException("No se permiten números negativos");
+		} else {
+			this.tiempoDisponible = tiempoDisponible;
+		}
+
+	}
+
 	/**
-	 * Guarda las compras realizadas por el usuario para incluirlas en el
-	 * archivo final.
+	 * Guarda las compras realizadas por el usuario para incluirlas en el archivo
+	 * final.
+	 * 
 	 * @return miItinerario
 	 */
-	
+
 	public Itinerario generarItinerario() {
 		Itinerario miItinerario = new Itinerario(this, this.getComprasPromociones(), this.getComprasAtracciones());
 		return miItinerario;
 	}
-	
+
 	public boolean comproAlgo() {
 		return !this.getComprasAtracciones().isEmpty() && !this.getComprasPromociones().isEmpty();
 	}
@@ -141,5 +166,5 @@ public class Usuario {
 				&& presupuesto == other.presupuesto
 				&& Double.doubleToLongBits(tiempoDisponible) == Double.doubleToLongBits(other.tiempoDisponible);
 	}
-	
+
 }
--- src/testers/UsuarioTest.java
+++ src/testers/UsuarioTest.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import clases.Tipo_De_Atraccion;
+import clases.Usuario;
+
 public class UsuarioTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void creaUsuarioTest() {
+		Usuario user = new Usuario("Jorge", 18, 2.5, "Aventura");
+		assertNotNull(user);
 	}
-
+	
+	@Test
+	public void chequeoPresupuestoUsuarioTest() {
+		Usuario user = new Usuario("Jorge", 18, 2.5, "Aventura");
+		assertEquals(18, user.getPresupuesto());
+		assertEquals(2.5, user.getTiempoDisponible(),0.01);
+		assertEquals(Tipo_De_Atraccion.AVENTURA, user.getPreferencia());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void chequeoControlSetPresupuestoTest() {
+		Usuario user2 = new Usuario("Juan", -2, 2.5, "Degustacion");
+	}
+	
 }
