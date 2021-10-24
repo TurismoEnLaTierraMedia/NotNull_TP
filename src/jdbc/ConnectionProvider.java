bf1db730b65a4f63ab3e340ebdaea962dd816c2e
CristianManu <adorelis0704@gmail.com>
2021-10-24 16:41:59 -0300

    nombrada correctamente la base de datos.

--- src/jdbc/ConnectionProvider.java
+++ src/jdbc/ConnectionProvider.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
-	private static String url = "jdbc:sqlite:users.db";
+	private static String url = "jdbc:sqlite:TurismoEnLaTierraMedia.db";
 	private static Connection connection;
 	
 	public static Connection getConnection() throws SQLException {
