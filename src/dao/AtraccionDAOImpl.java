f94f9791b8c369c19d323376c8d8a81bae185cb7
CristianManu <adorelis0704@gmail.com>
2021-10-29 21:09:43 -0300

    Desarrollados los metodos insert, update y findAll de UsuarioDAO y AtraccionDAO

--- src/dao/AtraccionDAOImpl.java
+++ src/dao/AtraccionDAOImpl.java
@@ -1,16 +1,40 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import clases.Atraccion;
+import jdbc.ConnectionProvider;
 
 public class AtraccionDAOImpl implements AtraccionDAO {
 
 	@Override
 	public List<Atraccion> findAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String sql = "SELECT * FROM atracciones";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
+
+			List<Atraccion> atracciones = new ArrayList<Atraccion>();
+			while (resultados.next()) {
+				atracciones.add(aAtraccion(resultados));
+			}
+
+			return atracciones;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+
+	private Atraccion aAtraccion(ResultSet resultados) throws SQLException {
+		return new Atraccion(resultados.getString(2), Integer.parseInt(resultados.getString(3)),
+				Double.parseDouble(resultados.getString(4)), Integer.parseInt(resultados.getString(5)),
+				resultados.getString(6));
 	}
 
 	@Override
@@ -20,15 +44,40 @@ public class AtraccionDAOImpl implements AtraccionDAO {
 	}
 
 	@Override
-	public int insert(Atraccion t) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insert(Atraccion atracc) throws SQLException {
+		try {
+			String sql = "INSERT OR IGNORE INTO atracciones (nombre, costo, tiempo, cupo, tipo) VALUES (?, ?, ?, ?, ?)";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, atracc.getNombre());
+			statement.setInt(2, atracc.getCostoDeVisita());
+			statement.setDouble(3, atracc.getDuracion());
+			statement.setInt(4, atracc.getCupo());
+			statement.setString(5, atracc.getTipo().toString());
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
 	@Override
-	public int update(Atraccion t) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int update(Atraccion atracc) throws SQLException {
+		try {
+			String sql = "UPDATE atracciones SET cupo = ? " + " WHERE nombre = ?";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, atracc.getCupo());
+			statement.setString(2, atracc.getNombre());
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
 	@Override
--- src/dao/UsuarioDAOImpl.java
+++ src/dao/UsuarioDAOImpl.java
@@ -2,18 +2,39 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import clases.Usuario;
 import jdbc.ConnectionProvider;
 
-public class UsuarioDAOImpl implements UsuarioDAO{
+public class UsuarioDAOImpl implements UsuarioDAO {
 
 	@Override
 	public List<Usuario> findAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String sql = "SELECT * FROM usuarios";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
+
+			List<Usuario> usuarios = new ArrayList<Usuario>();
+			while (resultados.next()) {
+				usuarios.add(aUsuario(resultados));
+			}
+
+			return usuarios;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+	
+	//Ver tema id
+	private Usuario aUsuario(ResultSet resultados) throws SQLException {
+		return new Usuario(resultados.getString(2), Integer.parseInt(resultados.getString(3)),
+				Double.parseDouble(resultados.getString(4)), resultados.getString(5));
 	}
 
 	@Override
@@ -25,7 +46,7 @@ public class UsuarioDAOImpl implements UsuarioDAO{
 	@Override
 	public int insert(Usuario usu) throws SQLException {
 		try {
-			String sql = "INSERT INTO usuarios (nombre, costo, tiempodisponible, preferencia) VALUES (?, ?, ?, ?)";
+			String sql = "INSERT OR IGNORE INTO usuarios (nombre, presupuesto, tiempodisponible, preferencia) VALUES (?, ?, ?, ?)";
 			Connection conn = ConnectionProvider.getConnection();
 
 			PreparedStatement statement = conn.prepareStatement(sql);
@@ -42,9 +63,21 @@ public class UsuarioDAOImpl implements UsuarioDAO{
 	}
 
 	@Override
-	public int update(Usuario t) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int update(Usuario usu) {
+		try {
+			String sql = "UPDATE usuarios SET presupuesto = ? , " + "tiempodisponible = ? " + " WHERE nombre = ?";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, usu.getPresupuesto());
+			statement.setDouble(2, usu.getTiempoDisponible());
+			statement.setString(3, usu.getNombre());
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
 	@Override
